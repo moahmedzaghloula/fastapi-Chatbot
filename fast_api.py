504cabe1c979ef9eca8ba480a358d43d13b85e96
muhammed amr zaghloula <133840307+moahmedzaghloula@users.noreply.github.com>
2024-05-10 20:32:33 +0300

    Initial commit

---
+++ fast_api.py
@@ -0,0 +1,92 @@
+from fastapi import FastAPI, WebSocket
+from pydantic import BaseModel
+import random
+import json
+from keras.models import load_model
+import numpy as np
+import pickle
+from nltk.stem import WordNetLemmatizer
+import nltk
+
+nltk.download('punkt')
+nltk.download('wordnet')
+
+lemmatizer = WordNetLemmatizer()
+model = load_model('model.h5')
+
+with open('data.json', 'r', encoding='utf-8') as file:
+    intents = json.load(file)
+
+words = pickle.load(open('texts.pkl', 'rb'))
+classes = pickle.load(open('labels.pkl', 'rb'))
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+def bow(sentence, words, show_details=True):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print("found in bag: %s" % w)
+    return np.array(bag)
+
+def predict_class(sentence, model):
+    p = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if (i['tag'] == tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
+
+app = FastAPI()
+
+class Message(BaseModel):
+    msg: str
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        response = chatbot_response(data)
+        await websocket.send_text(response)
+
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to the chatbot API!"}
+
+@app.post("/predict")
+async def predict(message: Message):
+    response = chatbot_response(message.msg)
+    return {"response": response}
+
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, port=5012)
+
+
